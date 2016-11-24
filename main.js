1222e552b030aeb22e7189857215d8936ebe1cb0
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2016-11-23 21:38:19 -0500

    continuing style updates

--- main.js
+++ main.js
@@ -6,24 +6,19 @@ var lineChart = new Chart(traffic, {
         labels: ["S", "M", "T", "W", "TH", "F", "S"],
         datasets: [{
             label: 'Traffic',
-            data: [12, 19, 3, 5, 2, 3, 1],
+            data: [500, 2200, 1900, 1200, 900, 450, 1333],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(187, 222, 251, 0.5)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(25, 118, 210, 0.5)'
             ],
-            borderWidth: 1
+            borderWidth: 1,
+            lineTension: 0.05,
+            pointBorderWidth: 3,
+            pointRadius: 6,
+            pointBorderColor: 'rgba(25, 118, 210, 0.5)',
+            pointBackgroundColor: 'white'
         }]
     },
     options: {
