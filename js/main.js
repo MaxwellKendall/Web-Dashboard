4c3b20db4052287c8bf549e75b4a4b3b2b2d9180
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2016-12-12 14:10:59 -0500

    almost done

--- js/main.js
+++ js/main.js
@@ -91,6 +91,80 @@ email.addEventListener('keyup', function(){
     }
 })
 
+// Setting Local Storage
+
+var checkBoxes = [
+	document.getElementById('notifications'),
+	document.getElementById('privacy'),
+	document.getElementById('xyz')
+];
+// for (var i = 0; i < checkBoxes.length; i ++){
+// 	checkBoxes[i].addEventListener("mouseover", saveSettings);
+// }
+
+var saveSettingsButton = document.getElementsByClassName('form2')[0];
+saveSettingsButton.addEventListener('submit', saveSettings);
+
+function saveSettings(){
+	event.preventDefault();
+
+	// 1. Saving the Settings Selections
+
+		localStorage.email = checkBoxes[0].checked;
+		localStorage.privacy = checkBoxes[1].checked;
+		localStorage.timezone = checkBoxes[2].selectedIndex;
+		console.log("localStorage.email = " + localStorage.email + " actual state = " + checkBoxes[0].checked);
+		console.log("privacy = " + localStorage.privacy + " actual state = " + checkBoxes[1].checked);
+		console.log("localStorage.timezone = " + localStorage.timezone + " actual state = " + checkBoxes[2].selectedIndex);
+
+	// 2. Adding the overlay
+
+	    var success = document.createElement("div");
+	    success.setAttribute("id", "successmessage");
+	    success.innerHTML = "<div class='innersuccess'><h1>Your settings have been successfully saved</h1><input id='confirm' type='submit' value='OK'></input></div>";
+	    var container = document.getElementsByClassName('container')[0];
+	    container.insertAdjacentElement("afterbegin", success);
+
+	// 2 b. removing overlay
+
+		var x = [
+			document.getElementById('confirm'),
+			document.getElementById("successmessage")
+		];
+			for (var i = 0; i < x.length; i ++){
+				x[i].addEventListener("click", removeOverlay);
+			}
+		function removeOverlay(){
+			console.log('removeoverlay function triggered');
+			x[1].remove();
+		}
+
+}
+
+// Actually define the Settings Values via local Storage
+
+function setSettings(){
+	console.log("setSettings function triggered");
+
+if (localStorage.length > 0) {
+		if (localStorage.email === "true") {
+			checkBoxes[0].checked = true;
+		} else if (localStorage.email === "false"){
+			checkBoxes[0].checked = false;
+		};
+
+		if (localStorage.privacy === "true"){
+			console.log(localStorage.privacy + " is true");
+			checkBoxes[1].checked = true;
+		} else if (localStorage.privacy === "false"){
+			console.log(localStorage.privacy + " is false");
+			checkBoxes[1].checked = false;
+		}
+	checkBoxes[2].selectedIndex = localStorage.timezone;
+	}
+}
+
+document.addEventListener("DOMContentLoaded", setSettings);
 
 
 /******************** CHART STUFF ****************************************/
@@ -248,15 +322,7 @@ var barChart = new Chart(dailyTraffic, {
         datasets: [{
             label: 'Daily Traffic',
             data: [12, 19, 3, 5, 2, 3, 6],
-            backgroundColor: [
-                'rgba(187, 222, 251, 0.5)',
-                'rgba(187, 222, 251, 0.5)',
-                'rgba(187, 222, 251, 0.5)',
-                'rgba(187, 222, 251, 0.5)',
-                'rgba(187, 222, 251, 0.5)',
-                'rgba(187, 222, 251, 0.5)',
-                'rgba(187, 222, 251, 0.5)'
-            ],
+            backgroundColor: 'rgba(3, 169, 244, 0.5)',
             borderColor: [
                 'rgba(25, 118, 210, 0.5)',
                 'rgba(25, 118, 210, 0.5)',
@@ -267,7 +333,30 @@ var barChart = new Chart(dailyTraffic, {
                 'rgba(25, 118, 210, 0.5)'
                 ],
             borderWidth: 1
-        }]
+        },
+		{
+			label: 'Daily Average',
+			data: [15, 12, 7, 10, 9, 6, 10],
+			backgroundColor: [
+				'rgba(187, 222, 251, 0.5)',
+				'rgba(187, 222, 251, 0.5)',
+				'rgba(187, 222, 251, 0.5)',
+				'rgba(187, 222, 251, 0.5)',
+				'rgba(187, 222, 251, 0.5)',
+				'rgba(187, 222, 251, 0.5)',
+				'rgba(187, 222, 251, 0.5)'
+			],
+			borderColor: [
+				'rgba(25, 118, 210, 0.5)',
+				'rgba(25, 118, 210, 0.5)',
+				'rgba(25, 118, 210, 0.5)',
+				'rgba(25, 118, 210, 0.5)',
+				'rgba(25, 118, 210, 0.5)',
+				'rgba(25, 118, 210, 0.5)',
+				'rgba(25, 118, 210, 0.5)'
+				],
+			borderWidth: 1
+		}]
     },
     options: {
         tooltip: {
@@ -295,14 +384,14 @@ var mobileUsers = document.getElementById("pie");
 var pieChart = new Chart(mobileUsers, {
     type: 'doughnut',
     data: {
-        labels: ["Phones", "Tablets", "Desktop"],
+        labels: ["Phones", "Tablets", "Desktop", "Other"],
         datasets: [{
-            data: ['99', '55', '12'],
+            data: ['99', '55', '12', '25'],
             backgroundColor: [
                 'rgba(187, 222, 251, 0.5)',
                 'rgba(252, 179, 229, 0.5)',
-                'rgba(252, 202, 179, 0.5)'
-                // 'rgba(193, 252, 179, 0.5)',
+                'rgba(252, 202, 179, 0.5)',
+                'rgba(193, 252, 179, 0.5)'
                 // 'rgba(0, 188, 212, 0.5)',
                 // 'rgba(32, 163, 151, 0.5)'
 
@@ -311,7 +400,7 @@ var pieChart = new Chart(mobileUsers, {
                 'rgba(178, 235, 242, 0.5)',
                 'rgba(178, 235, 242, 0.5)',
                 'rgba(178, 235, 242, 0.5)',
-                // 'rgba(25, 118, 210, 0.5)',
+				'rgba(178, 235, 242, 0.5)'
                 // 'rgba(25, 118, 210, 0.5)',
                 // 'rgba(25, 118, 210, 0.5)',
                 // 'rgba(25, 118, 210, 0.5)'
